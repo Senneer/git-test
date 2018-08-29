a127cace1c921f0cc1052bb6b66ed76a9ff0aa34
Nina <oposym0045@gmail.com>
2018-08-30 00:47:08 +0700

    object

---
+++ obj.js
@@ -0,0 +1,41 @@
+const Animal = {
+  type: 'mammals',
+  skinCovering: function() { 
+    console.log('hairy');
+  }
+};
+
+const animal1 = Object.create(Animal);
+console.log(animal1.type);
+animal1.skinCovering(); 
+
+const fish = Object.create(Animal);
+fish.type = 'Fishes';
+console.log(fish.type);
+console.log(Animal.type);
+
+
+
+const obj={
+	b : 6,
+	a : 5,
+}
+
+function vah(obj, key){
+	return obj[key] * 3;
+}
+
+console.log(vah(obj, 'b'));
+
+
+
+
+function MyObject(id, name){
+	this._id = id;
+    this._name = name;
+} 
+
+var MyFirstObjectInstance = new MyObject(5,"Sample");
+var MySecondObjectInstace = new MyObject(12,"Othe Sample"); 
+
+console.log(MySecondObjectInstace);
