f4b6d58ec860828fff5e56126cb9bee318d16600
Nina Sekret <oposym0045@gmail.com>
2018-08-29 23:02:39 +0700

    Update cycles.js

--- cycles.js
+++ cycles.js
@@ -2,15 +2,15 @@ let i = 0;
 let y = 10;
 
 // while (i < 5){
-// 	console.log(i);
-// 	i++;
+//  console.log(i);
+//  i++;
 // };
 
 do {
-	console.log(y);
-	y--;
+  console.log(y);
+  y--;
 } while (y > 0);
 
 for (; i < 3; i++) {
-	console.log(i);
+  console.log(i);
 }
