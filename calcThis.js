cabb1d8d587fd11788969d83da4323dda002bf51
Nina <oposym0045@gmail.com>
2018-09-03 23:31:44 +0700

    mend

--- calcThis.js
+++ calcThis.js
@@ -1,15 +1,15 @@
-let calculator = {
-	read: function() {
-		this.a = parseFloat(prompt('Введите первое число', ''));
-		this.b = parseFloat(prompt('Введите второе число', ''));
-	},
-	sum: function() {
-		return this.a + this.b;
-	},
-	mul: function(){
-		return this.a * this.b;
-	}
-}
+const calculator = {
+    read: function() {
+        this.a = parseFloat(prompt('Введите первое число', ''));
+        this.b = parseFloat(prompt('Введите второе число', ''));
+    },
+    sum: function() {
+        return this.a + this.b;
+    },
+    mul: function(){
+        return this.a * this.b;
+    }
+};
 
 calculator.read();
 alert( calculator.sum() );
--- ladderThis.js
+++ ladderThis.js
@@ -1,17 +1,17 @@
-var ladder = {
-	step: 0,
-	up: function() {
-		this.step++;
-		return this;
-	},
-	down: function() {
-		this.step--;
-		return this;
-	},
-	showStep: function() {
-		alert( this.step );
-		return this;
-	}
+const ladder = {
+    step: 0,
+    up: function() {
+        this.step++;
+        return this;
+    },
+    down: function() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        alert( this.step );
+        return this;
+    }
 };
 
 ladder.up().up().down().up().down().showStep();
